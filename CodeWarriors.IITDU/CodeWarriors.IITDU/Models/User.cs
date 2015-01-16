59fb81e5b103215506254ac7dec35980deb52ee3
rifatbit0401 <rifat92iitdu@gmail.com>
2015-01-16 11:01:16 -0800

    Added User and UserProfile Model Classes

---
+++ CodeWarriors.IITDU/CodeWarriors.IITDU/Models/User.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CodeWarriors.IITDU.Models
+{
+    public class User
+    {
+        public int UserId { get; set; }
+        public String UserName { get; set; }
+        public String Password { get; set; }
+
+    }
+}
---
+++ CodeWarriors.IITDU/CodeWarriors.IITDU/Models/UserProfile.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CodeWarriors.IITDU.Models
+{
+    public class UserProfile
+    {
+        public int UserId { get; set; }
+        public String FirstName { get; set; }
+        public String LastName { get; set; }
+        public String Email { get; set; }
+        public String Gender { get; set; }
+        public int Age { get; set; }
+        public String Country { get; set; }
+        public String About { get; set; }
+    }
+}
