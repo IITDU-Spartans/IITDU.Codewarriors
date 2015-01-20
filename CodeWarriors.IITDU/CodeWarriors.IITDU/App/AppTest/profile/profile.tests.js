2f0292d66e3b90ae2e3482d36f8ddceac97f7b9a
NadiaIT <nadia.nahar.iit@gmail.com>
2015-01-20 21:31:30 +0600

    angular testing with karma and jasmine

---
+++ CodeWarriors.IITDU/CodeWarriors.IITDU/App/AppTest/profile/profile.tests.js
@@ -0,0 +1,22 @@
+﻿describe('ProfileController', function () {
+    var scope;
+    beforeEach(angular.mock.module('CodeWarriorsApp'));
+
+    beforeEach(angular.mock.inject(function ($rootScope, $controller, _$httpBackend_) {
+        $httpBackend = _$httpBackend_;
+        $httpBackend.when('GET', '/Profile/GetProfileInformation').
+            respond({ FirstName: "Sponge", LastName: 'Bob' });
+        scope = $rootScope.$new();
+        $controller('ProfileController', { $scope: scope });
+    }));
+
+    it('should have EditMode = "false"', function () {
+        expect(scope.EditMode).toBe(false);
+    });
+
+    it('should fetch profile info of user', function () {
+        $httpBackend.flush();
+        expect(scope.ProfileInfo.FirstName).toBe('Sponge');
+        expect(scope.ProfileInfo.LastName).toBe('Bob');
+    });
+});
